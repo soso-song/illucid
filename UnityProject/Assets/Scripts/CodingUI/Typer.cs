1e8f0fb206027042aaf03fd144b3b3fc30b6646c
Jiale Shang <jiale.shang@mail.utoronto.ca>
2023-02-15 19:47:57 -0500

    day1 coding scene

---
+++ UnityProject/Assets/Scripts/CodingUI/Typer.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typer : MonoBehaviour
+{
+    private int count = 0;
+
+    void Start()
+    {
+        LoadImage(0);
+    }
+
+
+    // Update is called once per frame
+    void LoadImage(int num)
+    {
+        
+    }
+}
---
+++ UnityProject/Assets/Scripts/GameManager/GameFlowControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFlowControl : MonoBehaviour
+{
+    void Start()
+    {
+        // load "open-eye" animation
+    }
+
+    public void LoadLevel()
+    {
+        //load "close-eye" animation
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1 );
+    }
+}
