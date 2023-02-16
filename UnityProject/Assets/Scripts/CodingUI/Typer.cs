195c822431de851424c920890e27b76ccfbe1a3f
Jiale Shang <jiale.shang@mail.utoronto.ca>
2023-02-15 20:32:23 -0500

    code typing interaction

--- UnityProject/Assets/Scripts/CodingUI/Typer.cs
+++ UnityProject/Assets/Scripts/CodingUI/Typer.cs
@@ -1,22 +1,52 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Typer : MonoBehaviour
 {
-    private int count = 0;
+    public string dirPath = string.Empty;
+    public RawImage img;
+    public GameObject GameManager;
+    GameFlowControl GameFlowControl;
+    int count = 0;
+
+    Texture2D tex = null;
+    byte[] fileData;
 
     void Start()
     {
-        LoadImage(0);
+        dirPath = Application.dataPath + "/Code/"+SceneManager.GetActiveScene().name;
+        GameFlowControl = GameManager.GetComponent<GameFlowControl>();
+        LoadImage();
     }
 
 
     // Update is called once per frame
-    void LoadImage(int num)
+    void LoadImage()
     {
-        
+        string filePath = dirPath+"/"+count.ToString()+".png";
+        Debug.Log(filePath);
+        if (File.Exists(filePath)){
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2,2);
+            tex.LoadImage(fileData);
+            img.texture = tex;
+        }else{
+            Debug.Log("next level");
+            // load animation here
+            GameFlowControl.LoadLevel();
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown){
+            count++;
+            LoadImage();
+        }
     }
 }
