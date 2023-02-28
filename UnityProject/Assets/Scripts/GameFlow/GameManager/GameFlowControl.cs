146da851aca537dc9b6c4c8cdcbd1e80c7ca1c1a
Jiale Shang <jiale.shang@mail.utoronto.ca>
2023-02-28 16:50:01 -0500

    blink eye effect  UI mask

--- UnityProject/Assets/Scripts/GameFlow/GameManager/GameFlowControl.cs
+++ UnityProject/Assets/Scripts/GameFlow/GameManager/GameFlowControl.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameFlowControl : MonoBehaviour
 {
+    public RawImage mask;
     void Start()
     {
-        // load "open-eye" animation
+        // play mask animation
+        StartCoroutine(playBlinkAnimation(1));
+    }
+
+    private IEnumerator playBlinkAnimation(float dir)
+    {
+        // play mask animation
+        Animator anim = mask.GetComponent<Animator>();
+        anim.SetFloat("Direction", dir);
+        anim.Play("BlinkEyeAnim");
+        yield return new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !anim.IsInTransition(0));
     }
 
     public void LoadLevel()
     {
-        //load "close-eye" animation
+        // reverse mask animation
+        StartCoroutine(playBlinkAnimation(-1));
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1 );
     }
 
