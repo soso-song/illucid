bf5cc61bcd77ff58d7849ff74d189a7adb0fc754
Soso Song <shang8024@gmail.com>
2023-02-28 17:58:48 -0500

    close/open eye transition between levels

--- UnityProject/Assets/Scripts/GameFlow/GameManager/GameFlowControl.cs
+++ UnityProject/Assets/Scripts/GameFlow/GameManager/GameFlowControl.cs
@@ -3,30 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor;
+
 
 public class GameFlowControl : MonoBehaviour
 {
     public RawImage mask;
+    bool isloading = false;
+    float timeElapsed = 0f;
     void Start()
     {
-        // play mask animation
-        StartCoroutine(playBlinkAnimation(1));
+        // play open eye animation-
+        if (mask == null){
+            if (GameObject.Find("Mask") == null){
+                // create a new mask from perfab
+                GameObject maskObj = Instantiate(AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Mask.prefab", typeof(GameObject))) as GameObject;
+                mask = maskObj.GetComponent<RawImage>();
+                // add to canvas
+                maskObj.transform.SetParent(GameObject.Find("Canvas").transform, false);
+
+
+
+            } else{
+                mask = GameObject.Find("Mask").GetComponent<RawImage>();
+            }
+        }
+        StartCoroutine(playBlinkAnimation("OpenEyeAnim"));
     }
 
-    private IEnumerator playBlinkAnimation(float dir)
+    IEnumerator playBlinkAnimation(string clip)
     {
-        // play mask animation
         Animator anim = mask.GetComponent<Animator>();
-        anim.SetFloat("Direction", dir);
-        anim.Play("BlinkEyeAnim");
-        yield return new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !anim.IsInTransition(0));
+        anim.Play(clip);
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length+0.5f);
     }
 
     public void LoadLevel()
     {
-        // reverse mask animation
-        StartCoroutine(playBlinkAnimation(-1));
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1 );
+        StartCoroutine(playBlinkAnimation("CloseEyeAnim"));
+        isloading = true;
+    }
+
+    void Update()
+    {
+        if (isloading)
+        {
+            timeElapsed += Time.deltaTime;
+            // if no animation is playing, load next scene
+            if (timeElapsed > 1.5f){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
     }
 
     public void QuitGame(){
