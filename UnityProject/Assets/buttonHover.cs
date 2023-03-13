fe641a161310e91226ea73bddc0b15d3a722119f
Jiale Shang <jiale.shang@mail.utoronto.ca>
2023-03-13 19:18:32 -0400

    add pause menu in intro level & day1 level

--- UnityProject/Assets/buttonHover.cs
+++ UnityProject/Assets/buttonHover.cs
@@ -8,20 +8,33 @@ public class buttonHover : MonoBehaviour
     public Image buttonSpanImage;
     public Color originalColor;
     public Color hoverColor;
+    private Button button;
+    private ColorBlock colorBlock;
+    private Color originalColorBlock;
+    private Color hoverColorBlock;
     // Start is called before the first frame update
     void Start()
     {
         buttonSpanImage = GetComponent<Image>();
         originalColor = buttonSpanImage.color;
+        button = GetComponent<Button>();
+        colorBlock = button.colors;
+        originalColorBlock = colorBlock.selectedColor;
+        hoverColorBlock = colorBlock.highlightedColor;
     }
 
     public void changeWhenHover()
     {
         buttonSpanImage.color = hoverColor;
+        colorBlock.selectedColor = hoverColorBlock;
+        button.colors = colorBlock;
     }
 
     public void changeWhenNotHover()
     {
         buttonSpanImage.color = originalColor;
+        // change button color back to original color
+        colorBlock.selectedColor = originalColorBlock;
+        button.colors = colorBlock;
     }
 }
